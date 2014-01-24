1c2f8b54e14bf412c39370bce0b3364374cd7d4e
Rajesh Akkineni <rajesh@vimukti.com>
2014-01-24 09:05:05 +0530

    Added registration

--- api.go
+++ api.go
@@ -1,51 +1,87 @@
 package main
 
 import (
-    "net/http"
+	"encoding/json"
+	"github.com/nu7hatch/gouuid"
+	"log"
+	"net/http"
 )
 
+type RegistrationResp struct {
+	Guid string
+}
 
 // This handler will now serve HTTP, HTTPS, and SPDY requests.
-func (*Server) HomeHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello, HTTP!"))
+func (server *Server) Register(w http.ResponseWriter, r *http.Request) {
+	session := server.db.New()
+	c := session.DB("lsp").C("users")
+	w.Header().Set("Content-Type", "application/json")
+
+	id, err := uuid.NewV4()
+	user := User{}
+	user.id = id.String()
+	err = c.Insert(user)
+	if err != nil {
+		log.Println(err)
+	}
+	resp := RegistrationResp{}
+	resp.Guid = user.id
+	enc := json.NewEncoder(w)
+	if err := enc.Encode(&resp); err != nil {
+		log.Println(err)
+	}
+	session.Close()
 }
-// This handler will now serve HTTP, HTTPS, and SPDY requests.
-func (*Server) Register(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello, HTTP!"))
+func (server *Server) UpdateUserDetails(w http.ResponseWriter, r *http.Request) {
+	session := server.db.New()
+	c := session.DB("lsp").C("users")
+	w.Header().Set("Content-Type", "application/json")
+
+	id, err := uuid.NewV4()
+	user := User{}
+	user.id = id.String()
+	err = c.Insert(user)
+	if err != nil {
+		log.Println(err)
+	}
+	resp := RegistrationResp{}
+	resp.Guid = user.id
+	enc := json.NewEncoder(w)
+	if err := enc.Encode(&resp); err != nil {
+		log.Println(err)
+	}
+	session.Close()
 }
-func (*Server) UpdateUserDetails(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello, HTTP!"))
+func (server *Server) Feed(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, HTTP!"))
 }
-func (*Server) Feed(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello, HTTP!"))
+func (server *Server) FeedSince(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, HTTP!"))
 }
-func (*Server) FeedSince(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello, HTTP!"))
-}
-func (*Server) GalleryPhotos(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello, HTTP!"))
+func (server *Server) GalleryPhotos(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, HTTP!"))
 }
 
-func (*Server) GalleryPhotosSince(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello, HTTP!"))
+func (server *Server) GalleryPhotosSince(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, HTTP!"))
 }
 
-func (*Server) GalleryVideos(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello, HTTP!"))
+func (server *Server) GalleryVideos(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, HTTP!"))
 }
 
-func (*Server) GalleryVideosSince(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello, HTTP!"))
+func (server *Server) GalleryVideosSince(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, HTTP!"))
 }
 
-func (*Server) Events(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello, HTTP!"))
+func (server *Server) Events(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, HTTP!"))
 }
 
-func (*Server) EventsSince(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello, HTTP!"))
+func (server *Server) EventsSince(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, HTTP!"))
 }
 
-func (*Server) Leaders(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello, HTTP!"))
+func (server *Server) Leaders(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, HTTP!"))
 }
--- server.go
+++ server.go
@@ -1,60 +1,83 @@
 package main
 
 import (
-    "github.com/SlyMarbo/spdy" // Import SPDY.
-    "labix.org/v2/mgo"
-    "github.com/huandu/facebook"
-    "github.com/gorilla/mux"
-    "fmt"
+	"fmt"
+	"github.com/SlyMarbo/spdy" // Import SPDY.
+	"github.com/gorilla/mux"
+	"github.com/huandu/facebook"
+	"labix.org/v2/mgo"
+	"log"
+	"net/http"
 )
 
-type Server struct{}
+type Server struct {
+	db *mgo.Session
+}
 
-func SetUpRoutes(server *Server){
+type User struct {
+	id    string
+	name  string
+	email string
+}
 
+func SetUpRoutes(server *Server) {
 
-    r := mux.NewRouter()
-    s := r.PathPrefix("/api/v1/").Subrouter()
-    // API Handlers
-    s.HandleFunc("/register", server.Register);
-    s.HandleFunc("/update", server.UpdateUserDetails);
-    s.HandleFunc("/feed", server.Feed);
-    s.HandleFunc("/feed/since/{date}", server.FeedSince);
-    s.HandleFunc("/feed/since/{date}", server.FeedSince);
-    s.HandleFunc("/gallery/photos", server.GalleryPhotos);
-    s.HandleFunc("/gallery/photos/since/{date}", server.GalleryPhotosSince);
-    s.HandleFunc("/gallery/videos/", server.GalleryVideos);
-    s.HandleFunc("/gallery/videos/since/{date}", server.GalleryVideosSince);
-    s.HandleFunc("/events", server.Events);
-    s.HandleFunc("/events/since/{date}", server.Events);
-    s.HandleFunc("/leaders/{state}/", server.Leaders);
-    s.HandleFunc("/leaders/{state}/{district}/", server.Leaders);
-    s.HandleFunc("/leaders/{state}/{district}/{const}", server.Leaders);
+	r := mux.NewRouter()
+	s := r.PathPrefix("/api/v1/").Subrouter()
+	// API Handlers
+	s.HandleFunc("/register", server.Register)
+	s.HandleFunc("/update", server.UpdateUserDetails)
+	s.HandleFunc("/feed", server.Feed)
+	s.HandleFunc("/feed/since/{date}", server.FeedSince)
+	s.HandleFunc("/feed/since/{date}", server.FeedSince)
+	s.HandleFunc("/gallery/photos", server.GalleryPhotos)
+	s.HandleFunc("/gallery/photos/since/{date}", server.GalleryPhotosSince)
+	s.HandleFunc("/gallery/videos/", server.GalleryVideos)
+	s.HandleFunc("/gallery/videos/since/{date}", server.GalleryVideosSince)
+	s.HandleFunc("/events", server.Events)
+	s.HandleFunc("/events/since/{date}", server.Events)
+	s.HandleFunc("/leaders/{state}/", server.Leaders)
+	s.HandleFunc("/leaders/{state}/{district}/", server.Leaders)
+	s.HandleFunc("/leaders/{state}/{district}/{const}", server.Leaders)
 
-    //Admin Section
-    a := r.PathPrefix("/admin/").Subrouter()
-    a.HandleFunc("/login", server.AdminLogin);
+	//Admin Section
+	a := r.PathPrefix("/admin/").Subrouter()
+	a.HandleFunc("/login", server.AdminLogin)
+
+	http.Handle("/", r)
 }
 
 func main() {
-    server := Server{}
-    SetUpRoutes(&server)
-    
-    // Use spdy's ListenAndServe.
-    err := spdy.ListenAndServeTLS("localhost:443", "cert.pem", "key.pem", nil)
-    if err != nil {
-        // handle error.
-    }
-    
-    res, _ := facebook.Get("/akkineni.rajesh", nil)
-    fmt.Println("my facebook id is", res["id"])
-    // s := &http.Server{
-    //     Addr:           ":8080",
-    //     Handler:        myHandler,
-    //     ReadTimeout:    10 * time.Second,
-    //     WriteTimeout:   10 * time.Second,
-    //     MaxHeaderBytes: 1 << 20,
-    // }
-    // log.Fatal(s.ListenAndServe())
-    mgo.Dial("url")
+	server := Server{}
+
+	session, err := mgo.Dial("mongodb://lsptest:lsptest@troup.mongohq.com:10051/lsp")
+	if err != nil {
+		log.Println(err)
+	}
+
+	server.db = session
+
+	SetUpRoutes(&server)
+
+	{
+		// Use spdy's ListenAndServe.
+		err := spdy.ListenAndServeTLS("localhost:443", "cert.pem", "key.pem", nil)
+		if err != nil {
+			// handle error.
+			//log.Println(err)
+			http.ListenAndServe(":8080", nil)
+		}
+	}
+
+	res, _ := facebook.Get("/akkineni.rajesh", nil)
+	fmt.Println("my facebook id is", res["id"])
+	// s := &http.Server{
+	//     Addr:           ":8080",
+	//     Handler:        myHandler,
+	//     ReadTimeout:    10 * time.Second,
+	//     WriteTimeout:   10 * time.Second,
+	//     MaxHeaderBytes: 1 << 20,
+	// }
+	// log.Fatal(s.ListenAndServe())
+
 }
